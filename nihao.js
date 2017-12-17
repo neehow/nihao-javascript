d7e671354c68b98682fe287d57b93d3053b71b21
Neehow <zdthf@163.com>
2017-12-17 23:19:48 +0800

    Create nihao.js
    第一次提交，哈哈

---
+++ nihao.js
@@ -0,0 +1,9 @@
+// I put these snippets in one file just for ease of read, so there are redefinitions of variables.
+
+// randomly sort an array
+var arr = [1,2,3,4,5];
+arr.sort(function() { return Math.random() - 0.5; });
+
+// remove duplicate items from array (using ES6)
+var arr = [1, 1, 2, 3, 2, 3, 3, 4, 5];
+var arr2 = [...new Set(arr)];
