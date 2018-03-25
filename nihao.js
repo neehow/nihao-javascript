2cc0a64d61b0a4d4fa6ad00f4c9c862df71b8ad1
你好 <7135209+neehow@users.noreply.github.com>
2018-03-25 23:07:21 +0800

    add a way to copy an array

--- nihao.js
+++ nihao.js
@@ -1,9 +1,13 @@
-// I put these snippets in one file just for ease of read, so there are redefinitions of variables.
+// code snippets are put in one file just for ease of read, so there are redefinitions of variables.
 
 // randomly sort an array
-var arr = [1,2,3,4,5];
+var arr = [1, 2, 3, 4, 5];
 arr.sort(function() { return Math.random() - 0.5; });
 
 // remove duplicate items from array (using ES6)
 var arr = [1, 1, 2, 3, 2, 3, 3, 4, 5];
 var arr2 = [...new Set(arr)];
+
+// copy an array
+var arr = [1, 2, 3, 4, 5];
+var arr2 = arr.slice();
